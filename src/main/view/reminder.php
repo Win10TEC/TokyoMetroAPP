08d964a294855b18818067060aca07961e163009
Win10TEC <win10.labch@gmail.com>
2018-09-09 21:42:25 +0900

    viewの追加

---
+++ src/main/view/reminder.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <?php require_once dirname(__FILE__) . '/head.php'; ?>
+    <title>Reminder</title>
+</head>
+<body>
+<div class="container">
+    <h1><br>Reminder<br></h1>
+    <h1><br>3秒後にトップページへリダイレクトします。<br></h1>
+            <?php
+            header('Refresh: 3; URL=/index.php');
+            ?>
+</div>
+</body>
+</html>
---
+++ src/main/view/train.php
@@ -0,0 +1,46 @@
+<html>
+
+<head>
+    <?php
+    require_once dirname(__FILE__) . '/../view/head.php';
+    require_once dirname(__FILE__) . '/../controller/TrainController.php';
+    ?>
+</head>
+
+<body>
+
+<div class="container">
+    <h2>電車情報</h2>
+    <div class="row">
+        <div class="col-md-8">
+            <?php
+            echo '<div class="card">';
+            echo '<div class="card-body">';
+            foreach ($this->trainList as $item) {
+                echo 'date==>'. $item["date"].'<br><br>';
+                echo 'valid==>'.$item["valid"].'<br><br>';
+                echo 'frequency==>'.$item["frequency"].'<br><br>';
+                echo 'railway==>'.$item["railway"].'<br><br>';
+                echo 'sameAs==>'.$item["sameAs"].'<br><br>';
+                echo 'trainNumber==>'.$item["trainNumber"].'<br><br>';
+                echo 'delay==>'.$item["delay"].'<br><br>';
+                echo 'startingStation==>'.$item["startingStation"].'<br><br>';
+                echo 'terminalStation==>'.$item["terminalStation"].'<br><br>';
+                echo 'fromStation==>'.$item["fromStation"].'<br><br>';
+                echo 'toStation==>'.$item["toStation"].'<br><br>';
+                echo 'railDirection==>'.$item["railDirection"].'<br><br>';
+                echo 'trainOwner==>'.$item["trainOwner"].'<br><br>';
+
+                echo "<br>";
+                echo "<br>";
+            }
+            echo '</div>';
+            echo '</div>';
+            ?>
+        </div>
+    </div>
+
+
+</div>
+</body>
+</html>

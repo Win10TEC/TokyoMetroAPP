efe6ff7f1abb0dc2e3bc9cecaa378c350592aaab
Win10TEC <win10.labch@gmail.com>
2019-11-22 14:41:14 +0900

    add_config

---
+++ src/main/config/ApiConfig.php
@@ -0,0 +1,13 @@
+<?php
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
+$dotenv->load(__DIR__ . '/..');
+
+$mtrurl= getenv('METRO_URL');
+$mtrkey= getenv('METRO_TOKEN');
+$slackurl= getenv('SLACK_POST_URL');
+$slackkey= getenv('SLACK_TOKEN');
+
+define("METRO_URL", $mtrurl );
+define("METRO_TOKEN", $mtrkey );
+define("SLACK_POST_URL", $slackurl);
+define("SLACK_TOKEN", $slackkey);
